d91d5e48c90f312702026f039e787ced5a7759fa
mariarezende07 <maloasan@gmail.com>
2021-11-05 13:36:11 -0300

    ⏪ Revert the integration

--- kinematic_simulation.m
+++ kinematic_simulation.m
@@ -1,34 +1,38 @@
+clear all; clc; close all;
 %% Parameters 
 dt = 0.1; % Step size
 ts = 10; % Simulation time
 t = 0:dt:ts; % Time span
 
-%% Trajectory
-sliding_variable
 
 %% Initial conditions
-x_1_0=[[0;0;0];[0;0;0]];
-x_2_0=[[-1;0;0];[0;0;0]];
-x_3_0=[[0;-1;0];[0;0;0]];
-x_4_0=[[1;0;0];[0;0;0]];
-x_5_0=[[0;1;0];[0;0;0]];
+x0 = 0.5;
+y0=0.5;
+psi0 = pi/4;
 
-eta_1_0 = [x_1_0(1);x_1_0(2);x_1_0(3)];
+eta0=[x0;y0;psi0];
+eta(:,1) = eta0;
+
 
 for i = 1:length(t)
-    psi = eta_1_(3); % current orientation in rad
+    psi = eta(3,i); % current orientation in rad
 
-    R_1_psi = [cos(psi), -sin(psi),0; 
+    J_psi = [cos(psi), -sin(psi),0; 
               sin(psi), cos(psi), 0;
               0, 0 , 1];
-    u = 0.1; % x-axis velocity B frame
-    w = 0.5; % y-axis velocity B frame
-    r = 0.3; % angular velocity B frame
 
-    zeta(:,i) = [u;v;r];
+%% Desired states (Generalized coordinates)
+    eta_d = [2-2*cos(0.1*t(i));2*sin(0.1*t(i));0.1*t(i)];
+    eta_d_dot = [2*0.1*sin(0.1*t(i));2*0.1*cos(0.1*t(i));0.1];
 
+    %% Vector of velocity input commands
+    zeta(:,i) = inv(J_psi) * eta_d_dot;
+
+    % Time derivative of generalized coordinates 
     eta_dot(:,i) = J_psi * zeta(:,i);
-    eta(:,i+1) = eta(:,i) + dt*eta_dot(:,i); % Euler-method
+
+%% Position propagation using Euler method
+    eta(:,i+1) = eta(:,i) + dt*eta_dot(:,i); % State update
 end     
 %% Mobile robot animation
 l = 0.6; %length of the mobile robot
@@ -38,19 +42,17 @@ mr_co = [-l/2, l/2, l/2, -l/2, -l/2;
          -w/2, -w/2, w/2, w/2, -w/2;];
 
 figure
-for j = n_agents
-    for i = 1:length(t)
-        psi = eta(3,i); % current orientation in rad
+for i = 1:length(t)
+    psi = eta(3,i); % current orientation in rad
+    R_psi = [cos(psi), -sin(psi); 
+               sin(psi), cos(psi);];
+    v_pos = R_psi * mr_co;
 
-        R_psi = [cos(psi), -sin(psi); 
-                sin(psi), cos(psi);];
-        v_pos = R_psi * mr_co;
-
-        fill(v_pos(1,:)+eta(1,i),v_pos(2,:)+eta(2,i),'g')
-        hold on, grid on; axis([-1 3 -1 3]), axis square
-        plot(eta(1,1:i),eta(2,1:i),'b-');
-        legend('MR', 'Path'), set(gca, 'fontsize',24)
-        xlabel('x[m]'); ylabel('y[m]');
-        pause(0.1); hold off
-    end     
-end
+    fill(v_pos(1,:)+eta(1,i),v_pos(2,:)+eta(2,i),'g')
+    hold on, grid on; 
+    axis([-1 3 -1 3]), axis square
+    plot(eta(1,1:i),eta(2,1:i),'b-');
+    legend('MR', 'Path'), set(gca, 'fontsize',24)
+    xlabel('x[m]'); ylabel('y[m]');
+    pause(0.1); hold off
+end     
