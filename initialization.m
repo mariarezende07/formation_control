311e6ec13342583ad9062e118dfda75f79ad6b65
mariarezende07 <maloasan@gmail.com>
2022-02-03 02:24:18 -0300

    ✏️ Rename agent 1 to leader

--- initialization.m
+++ initialization.m
@@ -10,13 +10,12 @@ sliding_variable
 
 %% PARAMETERS
 
-clear A_c nu R M f 
+clear A_c b_c nu R M f 
 
 %% Initial position
-
-x_SS_1_0=[[0;0;0];[0;0;0]];
-x_SS_2_0=[[-1;0;0];[0;0;0]];
-x_SS_3_0=[[0;-1;0];[0;0;0]];
+x_SS_leader_0=[[6;0;0];[0;0;0]];
+x_SS_2_0=[[0;0;0];[0;0;0]];
+x_SS_3_0=[[8;0;0];[0;0;0]];
 
 %% Control (cooperative reference filter)
 
@@ -28,14 +27,14 @@ m = 10;
 
 %% Desired trajectory
 
-x_0_points=[0;10;10];
-x_T_points=[10;10;0;];
+x_0_points=[10;10;10];
+x_T_points=[10;10;10];
 
-y_0_points=[0;0;10;];
-y_T_points=[0;10;10;];
+y_0_points=[10;10;10];
+y_T_points=[10;10;10];
 
-theta_0_points=-[0;0;pi/2;];
-theta_T_points=-[0;pi/2;pi;];
+theta_0_points=[0;0;0];
+theta_T_points=[0;0;0];
 
 psi_trajectory=0;
 
@@ -49,17 +48,9 @@ T = final_time/length(x_0_points);
 
 trajectory_calc
 
-%% Plot
-% figure
-% plot(time_vector,sigma_1(1,:));
-% figure
-% plot(time_vector,sigma_1(2,:));
-% figure
-% plot(time_vector,sigma_1(3,:));
-% 
-% figure
-% plot(x_SS_1(1,:),x_SS_1(2,:));
-% 
-% hold on;
-% plot(x_SS_2(1,:),x_SS_2(2,:));
-% plot(x_SS_3(1,:),x_SS_3(2,:));
+%% Ploting
+figure
+plot(x_SS_leader(1,:),x_SS_leader(2,:));
+hold on
+plot(x_SS_2(1,:),x_SS_2(2,:));
+plot(x_SS_3(1,:),x_SS_3(2,:));
--- trajectory_calc.m
+++ trajectory_calc.m
@@ -1,15 +1,10 @@
 N = length(time_vector);
 
-x_SS_1 = zeros(6,N);
+x_SS_leader = zeros(6,N);
 x_SS_2 = zeros(6,N);
 x_SS_3 = zeros(6,N);
 
-x_SS_leader = zeros(6,N);
-
 eta_desired = zeros(3,N);
-
-sigma_1 = zeros(3,N);
-Q_1 = zeros(3,N);
 
 sigma_2 = zeros(3,N);
 Q_2 = zeros(3,N);
@@ -19,29 +14,25 @@ Q_3 = zeros(3,N);
 
 x_SS_alpha = [0;0];
 
-x_SS_1(:,1) = x_SS_1_0;
+x_SS_leader(:,1) = x_SS_leader_0;
 x_SS_2(:,1) = x_SS_2_0;
 x_SS_3(:,1) = x_SS_3_0;
 
 for i=1:N-1
     
-    eta_desired(:,i) = trajectory(time_vector(i),x_SS_alpha,x_0_points,x_T_points,y_0_points,y_T_points,theta_0_points,theta_T_points,T,psi_trajectory);
+    eta_desired(:,i) = trajectory(time_vector(i),x_SS_alpha,x_0_points,x_T_points,y_0_points,y_T_points,...
+    theta_0_points,theta_T_points,T,psi_trajectory);
 
     x_SS_leader_dot = leader(x_SS_leader(:,i),eta_desired(:,i));
     x_SS_leader(:,i+1) = x_SS_leader(:,i) + dt * x_SS_leader_dot;
-    
-    [sigma_1(:,i+1),Q_1(:,i+1),x_SS_1_dot] = subsystem_trajectory_1(1,x_SS_1(:,i),x_SS_2(:,i),x_SS_3(:,i),...
-    lambda_sigma,k_sigma,l_sigma,epsilon_sigma);
 
-    x_SS_1(:,i+1) = x_SS_1(:,i) + dt * x_SS_1_dot;
-
-    [sigma_2(:,i+1),Q_2(:,i+1),x_SS_2_dot] = subsystem_trajectory_1(2,x_SS_1(:,i),x_SS_2(:,i),x_SS_3(:,i),...
-    lambda_sigma,k_sigma,l_sigma,epsilon_sigma);
+    [sigma_2(:,i+1),Q_2(:,i+1),x_SS_2_dot] = subsystem_trajectory_1(2,x_SS_leader(:,i),x_SS_2(:,i),x_SS_3(:,i),...
+                                                                    lambda_sigma,k_sigma,l_sigma,epsilon_sigma);
 
     x_SS_2(:,i+1) = x_SS_2(:,i) + dt * x_SS_2_dot;
     
-    [sigma_3(:,i+1),Q_3(:,i+1),x_SS_3_dot] = subsystem_trajectory_1(3,x_SS_1(:,i),x_SS_2(:,i),x_SS_3(:,i),...
-    lambda_sigma,k_sigma,l_sigma,epsilon_sigma);
+    [sigma_3(:,i+1),Q_3(:,i+1),x_SS_3_dot] = subsystem_trajectory_1(3,x_SS_leader(:,i),x_SS_2(:,i),x_SS_3(:,i),...
+                                                                    lambda_sigma,k_sigma,l_sigma,epsilon_sigma);
 
     x_SS_3(:,i+1) = x_SS_3(:,i) + dt * x_SS_3_dot;
     
