85b8896a1e0957a77b6f04e29c154c749bd7a581
mariarezende07 <maloasan@gmail.com>
2022-02-17 08:11:14 -0300

    ✨ Quadrangular formation

--- trajectory_calc.m
+++ trajectory_calc.m
@@ -3,6 +3,7 @@ N = length(time_vector);
 x_SS_leader = zeros(6,N);
 x_SS_2 = zeros(6,N);
 x_SS_3 = zeros(6,N);
+x_SS_4 = zeros(6,N);
 
 eta_desired = zeros(3,N);
 
@@ -12,28 +13,37 @@ Q_2 = zeros(3,N);
 sigma_3 = zeros(3,N);
 Q_3 = zeros(3,N);
 
+sigma_4 = zeros(3,N);
+Q_4 = zeros(3,N);
+
 x_SS_alpha = [0;0];
 
 x_SS_leader(:,1) = x_SS_leader_0;
 x_SS_2(:,1) = x_SS_2_0;
 x_SS_3(:,1) = x_SS_3_0;
+x_SS_4(:,1) = x_SS_4_0;
 
 for i=1:N-1
     %% Circular trajectory
-    eta_desired(:,i) = circle_trajectory(time_vector(i),x_SS_alpha,x_0_points,y_0_points,psi_trajectory);
+    eta_desired(:,i) = line_trajectory(time_vector(i),x_SS_alpha,x_0_points,x_T_points,y_0_points,y_T_points,theta_0_points,theta_T_points,T,psi_trajectory);
 
     x_SS_leader_dot = leader(x_SS_leader(:,i),eta_desired(:,i));
     x_SS_leader(:,i+1) = x_SS_leader(:,i) + dt * x_SS_leader_dot;
 
-    [sigma_2(:,i+1),Q_2(:,i+1),x_SS_2_dot] = subsystem_trajectory_1(2,x_SS_leader(:,i),x_SS_2(:,i),x_SS_3(:,i),...
+    [sigma_2(:,i+1),Q_2(:,i+1),x_SS_2_dot] = subsystem_trajectory_1(2,x_SS_leader(:,i),x_SS_2(:,i),x_SS_3(:,i),x_SS_4(:,i),...
                                                                     lambda_sigma,k_sigma,l_sigma,epsilon_sigma);
 
     x_SS_2(:,i+1) = x_SS_2(:,i) + dt * x_SS_2_dot;
     
-    [sigma_3(:,i+1),Q_3(:,i+1),x_SS_3_dot] = subsystem_trajectory_1(3,x_SS_leader(:,i),x_SS_2(:,i),x_SS_3(:,i),...
+    [sigma_3(:,i+1),Q_3(:,i+1),x_SS_3_dot] = subsystem_trajectory_1(3,x_SS_leader(:,i),x_SS_2(:,i),x_SS_3(:,i),x_SS_4(:,i),...
                                                                     lambda_sigma,k_sigma,l_sigma,epsilon_sigma);
 
     x_SS_3(:,i+1) = x_SS_3(:,i) + dt * x_SS_3_dot;
+
+    [sigma_4(:,i+1),Q_4(:,i+1),x_SS_4_dot] = subsystem_trajectory_1(4,x_SS_leader(:,i),x_SS_2(:,i),x_SS_3(:,i),x_SS_4(:,i),...
+                                                                    lambda_sigma,k_sigma,l_sigma,epsilon_sigma);
+
+    x_SS_4(:,i+1) = x_SS_4(:,i) + dt * x_SS_4_dot;
     
     
 end
